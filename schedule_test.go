a42e2344059d35ae346ad3d78b610b9500e473a7
roylee0704 <roylee0704@gmail.com>
2016-06-06 17:51:32 +0800

    completed periodicnext

--- schedule_test.go
+++ schedule_test.go
@@ -1,6 +1,7 @@
 package gron
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -15,21 +16,74 @@ func TestPeriodicNext(t *testing.T) {
 		want   string
 	}{
 
+		//Wraps around days
+		{
+			time:   "Mon Jun 6 23:46 2016",
+			period: 15 * time.Minute,
+			want:   "Mon Jun 7 00:01 2016",
+		},
+		{
+			time:   "Mon Jun 6 23:59:40 2016",
+			period: 21 * time.Second,
+			want:   "Mon Jun 7 00:00:01 2016",
+		},
+		{
+			time:   "Mon Jun 6 23:30:19 2016",
+			period: 29*time.Minute + 41*time.Second,
+			want:   "Mon Jun 7 00:00:00 2016",
+		},
+		{
+			time:   "Mon Jun 6 23:46:20 2016",
+			period: 15*time.Minute + 40*time.Second,
+			want:   "Mon Jun 7 00:02:00 2016",
+		},
+
+		// Wrap around months
+		{
+			time:   "Mon Jun 6 16:49 2016",
+			period: 30 * xtime.Day, // adds 30 days, equates to #days in June.
+			want:   "Mon Jul 6 16:49 2016",
+		},
+
+		// Wrap around minute, hour, day, month, and year
+		{
+
+			time:   "Sat Dec 31 23:59:59 2016",
+			period: 1 * time.Second,
+			want:   "Sun Jan 1 00:00:00 2017",
+		},
+
 		// Round to nearest second on the period
 		{
-			time:   "Mon Jun 9 12:45 2016",
-			period: 15*xtime.Minute + 100*time.Nanosecond,
-			want:   "Mon Jun 9 13:00 2016",
+			time:   "Mon Jun 6 12:45 2016",
+			period: 15*time.Minute + 100*time.Nanosecond,
+			want:   "Mon Jun 6 13:00 2016",
 		},
+
+		// Round to 1 second if the duration is less
+		{
+			time:   "Mon Jun 6 17:38:01 2016",
+			period: 59 * time.Millisecond,
+			want:   "Mon Jun 6 17:38:02 2016",
+		},
+
+		// Round to nearest second when calculating the next time.
+		{"Mon Jun 6 17:38:01.009 2016", 15 * time.Minute, "Mon Jun 6 17:53:01 2016"},
+
+		// Round to nearest second for both
+		{"Mon Jun 6 17:38:01.009 2016", 15*time.Minute + 50*time.Nanosecond, "Mon Jun 6 17:53:01 2016"},
 	}
 
-	for _, test := range tests {
+	for i, test := range tests {
 
 		got := Every(test.period).Next(getTime(test.time))
 		want := getTime(test.want)
 
+		fmt.Println(got)
+		fmt.Println(want)
+
 		if got != want {
-			t.Errorf("%s, \"%s\": (want) %v != %v (got)", test.time, test.period, want, got)
+			t.Errorf("case[%d], %s, \"%s\": (want) %v != %v (got)", i, test.time, test.period, want, got)
 		}
 	}
 
