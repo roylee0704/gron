bfaa83f4e5f2cb3f0aceb676305bdb283a0df363
roylee0704 <roylee0704@gmail.com>
2016-06-07 17:10:50 +0800

    sketching up the skeleton of cron

---
+++ cron.go
@@ -0,0 +1,43 @@
+package gron
+
+// Cron provides a convenient interface for scheduling job such as to clean-up
+// database entry every month.
+//
+// Cron keeps track of any number of entries, invoking the associated func as
+// specified by the schedule. It may also be started, stopped and the entries
+// may be inspected.
+type Cron struct {
+	running bool
+	stop    chan struct{}
+}
+
+// New instantiates new Cron instant c.
+func New() *Cron {
+	return &Cron{
+		stop: make(chan struct{}),
+	}
+}
+
+// Start signals cron instant c to get up and running.
+func (c *Cron) Start() {
+	go c.run()
+}
+
+// run the scheduler...
+//
+// It needs to be private as it's responsible of synchronizing a critical
+// shared state: `running`.
+func (c *Cron) run() {
+
+	for {
+		select {
+		case <-c.stop:
+			return // terminate go-routine.
+		}
+	}
+}
+
+// Stop halts cron instant c from running.
+func (c *Cron) Stop() {
+	c.stop <- struct{}{}
+}
