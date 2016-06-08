e79362f3fcd4c33f64ae0d806e288c3c6dff8a7a
roylee0704 <roylee0704@gmail.com>
2016-06-08 11:09:29 +0800

    complete tc#1

--- cron_test.go
+++ cron_test.go
@@ -1,8 +1,10 @@
 package gron
 
+import "testing"
+
 // Test that invoking stop() before start() silently returns,
 // without blocking the stop channel
-func TestStopWithoutStart() {
+func TestStopWithoutStart(t *testing.T) {
 	cron := New()
 	cron.Stop()
 }
