8236f6247a6ddaa6e275aba81e307920ecd06f01
roylee0704 <roylee0704@gmail.com>
2016-06-09 12:39:27 +0800

    improve sort.Less() comment and algorithm

--- cron.go
+++ cron.go
@@ -22,12 +22,17 @@ type byTime []Entry
 func (b byTime) Len() int      { return len(b) }
 func (b byTime) Swap(i, j int) { b[i], b[j] = b[j], b[i] }
 
-// Less bubbles late time & zero time to the back of queue.
+// Less reports `earliest` time i should sort before j.
+// zero time is not `earliest` time.
 func (b byTime) Less(i, j int) bool {
 
 	if b[i].Next.IsZero() {
 		return false
 	}
+	if b[j].Next.IsZero() {
+		return true
+	}
+
 	return b[i].Next.Before(b[j].Next)
 }
 
